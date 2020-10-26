19a3ef20c433dbb680c33442178d74696af60d4d
damonbogich <damonbogich@gmail.com>
2020-10-26 12:25:55 -0400

    first one working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+let funBusHeader = document.querySelector('.logo-heading');
+//1. mouseover on funBus header
+funBusHeader.addEventListener('mouseover', (e) => {
+    console.log('style', e.target.style);
+    e.target.style.color = 'blue';
+});
+//2. 
+
+let navItems = document.querySelectorAll('nav-link');
+let homeNav = navItems[0] 
+homeNav.addEventListener('keydown', (e) => {
+    console.log('working');
+    e.target.style.backgroundColor = 'red';
+})
+
